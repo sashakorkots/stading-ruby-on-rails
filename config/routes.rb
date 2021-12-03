1bba05797cd53463382bb1da866e32821dade7e7
Olexandr Korkots <k.sasha.g@gmail.com>
2021-12-03 23:47:08 +0300

    Create question form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post "/questions", to: "questions#create"
+  get "/questions/new", to: "questions#new"
   get "/questions", to: "questions#index"
   root "pages#index"
 end
