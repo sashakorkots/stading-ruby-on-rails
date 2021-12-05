d5e465c5b69ea9182f2a22c6ae0b9aec15a6aad5
Olexandr Korkots <k.sasha.g@gmail.com>
2021-12-05 20:55:06 +0300

    Create delete actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,6 +28,13 @@ class QuestionsController < ApplicationController
             render :new
         end
     end
+
+    def destroy
+        @question = Question.find_by id: params[:id]
+        @question.destroy
+        redirect_to questions_path
+    end
+    
     
     
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :questions, only: [:index, :new, :create, :edit, :update]
+  resources :questions, only: [:index, :new, :create, :edit, :update, :destroy]
 
   root "pages#index"
 end
