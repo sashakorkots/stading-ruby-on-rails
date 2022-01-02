13542059a064e4419bc2b81fb3e19d6b929644d6
Olexandr Korkots <k.sasha.g@gmail.com>
2022-01-03 01:01:29 +0300

    Create user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :current_user
+
+  private
+
+  def current_user
+    @current_user = User.find_by( id: session[:user_id]) if session[:user_id].present?
+  end
+
+  def user_sign_in?
+    current_user.present?
+  end
+
+  helper_method :current_user, :user_sign_in?
 end
