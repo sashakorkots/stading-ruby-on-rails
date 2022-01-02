db23c42066af51c058c07c2264fb336fe9bb656e
Olexandr Korkots <k.sasha.g@gmail.com>
2022-01-02 17:10:32 +0300

    Add edit answer actions and view

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,17 @@
 class AnswersController < ApplicationController
   before_action :set_question
+  before_action :set_answer, extend: :create
+
+  def update
+    if @answer.update answer_params
+      flash[:success] = 'Answer updated'
+      redirect_to question_path @question
+    else
+      render :edit
+    end
+  end
+
+  def edit; end
 
   def create
     @answer = @question.answers.build answer_params
@@ -8,14 +20,13 @@ class AnswersController < ApplicationController
       redirect_to question_path(@question)
     else
       flash[:danger] = "Answer doesn`t created"
-      @answers = Answer.order created_at: :desc
+      @answers = @question.answers.order created_at: :desc
       render "questions/show"
     end
   end
 
   def destroy
-    @answer = @question.answers.find_by id: params[:id]
-    if @answer .destroy
+    if @answer.destroy
       flash[:success] = "Question deleted"
     else
       flash[:danger] = "Something went wrong"
@@ -23,7 +34,7 @@ class AnswersController < ApplicationController
     redirect_to question_path(@question)
   end
 
-  private 
+  private
 
   def answer_params
     params.required(:answer).permit(:body)
@@ -31,5 +42,9 @@ class AnswersController < ApplicationController
 
   def set_question
     @question = Question.find_by id: params[:question_id]
+  end
+
+  def set_answer
+    @answer = @question.answers.find params[:id]
   end
 end
