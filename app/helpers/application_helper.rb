5f633e8df42a13ec8f4cfe62c8bb2451f00a4903
Olexandr Korkots <k.sasha.g@gmail.com>
2022-01-02 19:40:54 +0300

    Add updating styles for selected navigation tad

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,16 @@
 module ApplicationHelper
+  def nav_tab(title, url, options = {})
+    current_page = options.delete :current_page
+    css_class = current_page == title ? 'fw-bold' : ''
+    options[:class] = options[:class] ? "#{options[:class]} #{css_class}" : css_class
+
+    link_to title, url, options
+  end
+
+  def currently_at(current_page)
+    render partial: 'shared/menu', locals: { current_page: current_page }
+  end
+
   def full_title(page_title = '')
     base_title = 'OForum'
     if page_title.present?
